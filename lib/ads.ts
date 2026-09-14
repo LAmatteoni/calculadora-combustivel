82620bc593eef32bc653143d320ea18ebce67d7b
Lucca Matteoni <lam.245@hotmail.com>
2026-09-14 11:17:35 -0300

    chore: add ESLint configuration and update .gitignore files

--- lib/ads.ts
+++ lib/ads.ts
@@ -1,4 +1,4 @@
-// TODO: substituir pelos IDs reais da conta AdSense (Publisher ID e slots dos 2 anúncios laterais).
-export const ADSENSE_CLIENT = 'ca-pub-0000000000000000';
+// TODO: substituir AD_SLOT_LEFT/RIGHT pelos IDs reais quando criar as 2 unidades de anúncio no AdSense.
+export const ADSENSE_CLIENT = 'ca-pub-3363157573775659';
 export const AD_SLOT_LEFT = '0000000000';
 export const AD_SLOT_RIGHT = '0000000000';
